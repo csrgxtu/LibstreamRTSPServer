b9113cca7339d425afbc3319fef0837ee3c3607b
csrgxtu <1246506786@qq.com>
2015-04-09 14:02:25 +0800

    RTSPClient Structure

---
+++ src/RTSPClient.py
@@ -0,0 +1,60 @@
+/**
+ * Author: Archer Reilly
+ * File: RTSPClient.py
+ * Date: 09/Apr/2015
+ * Desc: RTSP Client Lib
+ *
+ * Produced By CSRGXTU
+ */
+
+class RTSPClient(object):
+  host = None
+  port = None
+
+  def __init__(self, host, port):
+    pass
+
+  def options(self):
+    pass
+
+  def describe(self):
+    pass
+
+  def setup(self):
+    pass
+
+  def play(self):
+    pass
+
+  def pause(self):
+    pass
+
+  def record(self):
+    pass
+
+  def announce(self):
+    pass
+
+  def teardown(self):
+    pass
+
+  def getParameter(self):
+    pass
+
+  def setParameter(self):
+    pass
+
+  def redirect(self):
+    pass
+
+  def getHost():
+    pass
+
+  def getPort():
+    pass
+
+  def setHost():
+    pass
+
+  def setPort():
+    pass
