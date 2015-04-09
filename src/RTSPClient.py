33fd4795a0afa3eaba203e4b3dc098e2dd965f2d
csrgxtu <1246506786@qq.com>
2015-04-09 15:26:39 +0800

    RTSPClient workable

--- src/RTSPClient.py
+++ src/RTSPClient.py
@@ -1,42 +1,115 @@
-/**
- * Author: Archer Reilly
- * File: RTSPClient.py
- * Date: 09/Apr/2015
- * Desc: RTSP Client Lib
- *
- * Produced By CSRGXTU
- */
+#!/usr/bin/env python
+# Author: Archer Reilly
+# File: RTSPClient.py
+# Date: 09/Apr/2015
+# Desc: RTSP Client Lib
+#
+# Produced By CSRGXTU
+import socket
+import sys
 
 class RTSPClient(object):
   host = None
   port = None
+  socket = None
+  RTSPUrl = None
 
   def __init__(self, host, port):
-    pass
+    self.host = host
+    self.port = port
+    self.RTSPUrl = 'rtsp://' + host + ':' + str(port)
+
+    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.socket.settimeout(5)
+    res = self.socket.connect_ex((self.host, self.port))
+    if res != 0:
+      sys.exit(1)
 
   def options(self):
-    pass
+    options = 'OPTIONS ' + self.RTSPUrl + ' RTSP/1.0\nCSeq: 1\n\n'
+    print options
+
+    if self.socket.send(options) != len(options):
+      print 'ERROR: send error'
+
+    res = self.socket.recv(8192)
+    print res
+    return res
 
   def describe(self):
-    pass
+    describe = 'DESCRIBE ' + self.RTSPUrl + ' RTSP/1.0\nCSeq: 2\n\n'
+    print describe
+
+    if self.socket.send(describe) != len(describe):
+        print 'ERROR: send error'
+
+    res = self.socket.recv(16384)
+    print res
+    return res
 
   def setup(self):
-    pass
+    setup = 'SETUP ' + self.RTSPUrl + '/trackID=1 RTSP/1.0\n'\
+      + 'CSeq: 3\nTransport: RTP/AVP;unicast;client_port=8000-8001'\
+      + '\n\n'
+    print setup
+
+    if self.socket.send(setup) != len(setup):
+      print 'ERROR: send errror'
+
+    res = self.socket.recv(16384)
+    print res
+    return res
 
   def play(self):
-    pass
+    play = 'PLAY ' + self.RTSPUrl + ' RTSP/1.0\n'\
+      + 'CSeq: 4\nSession: 1185d20035702ca\n\n'
+    print play
+
+    if self.socket.send(play) != len(play):
+      print 'ERROR: send error'
+
+    res = self.socket.recv(8192)
+    print res
+    return res
 
   def pause(self):
-    pass
+    pause = 'PAUSE ' + self.RTSPUrl + ' RTSP/1.0\n'\
+      + 'CSeq: 5\nSession: 1185d20035702ca\n\n'
+    print pause
+
+    if self.socket.send(pause) != len(pause):
+      print 'ERROR: send error'
+
+    res = self.socket.recv(8192)
+    print res
+    return res
 
   def record(self):
-    pass
+    record = 'RECORD ' + self.RTSPUrl + ' RTSP/1.0\n'\
+      + 'CSeq: 6\nSession: 1185d20035702ca\n\n'
+    print record
+
+    if self.socket.send(record) != len(record):
+      print 'ERROR: send error'
+
+    res = self.socket.recv(8192)
+    print res
+    return res
 
   def announce(self):
     pass
 
   def teardown(self):
-    pass
+    teardown = 'TEARDOWN ' + self.RTSPUrl + ' RTSP/1.0\n'\
+      + 'CSeq: 7\nSession: 1185d20035702ca\n\n'
+    print teardown
+
+    if self.socket.send(teardown) != len(teardown):
+        print 'ERROR: send error'
+
+    res = self.socket.recv(8192)
+    print res
+    return res
 
   def getParameter(self):
     pass
@@ -48,13 +121,13 @@ class RTSPClient(object):
     pass
 
   def getHost():
-    pass
+    return self.host
 
   def getPort():
-    pass
+    return self.port
 
-  def setHost():
-    pass
+  def setHost(host):
+    self.host = host
 
-  def setPort():
-    pass
+  def setPort(port):
+    self.port = port
